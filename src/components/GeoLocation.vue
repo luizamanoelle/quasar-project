75daad37f163049cb68b58d423e18abb7729d28d
Luiza Manoelle <luizamanoelle@hotmail.com>
2026-01-08 10:23:47 -0300

    add mapa

--- src/components/GeoLocation.vue
+++ src/components/GeoLocation.vue
@@ -1,25 +1,75 @@
 <template>
   <div>
-    <p>google maps compoenent</p>
-    <button @click="getLocation()">Get Location</button>
-    {{ lat }} , {{ lng }}
+    <p>{{ lat }} , {{ lng }}</p>
+
+    <!--telinha do mapa-->
+    <div ref="mapContainer" style="width: 100%; height: 188px; max-height: 50vh"></div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
 defineOptions({ name: 'GeoLocation' });
 
 const lat = ref(0);
 const lng = ref(0);
+const map = ref<L.Map>();
+const mapContainer = ref<HTMLElement>();
+const marker = ref<L.Marker>();
+
+onMounted(() => {
+  if (mapContainer.value) {
+    map.value = L.map(mapContainer.value).setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+
+      attribution: '&copy; OpenStreetMap',
+    }).addTo(map.value);
+
+    getLocation();
+  }
+});
 
 function getLocation() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      lat.value = position.coords.latitude;
-      lng.value = position.coords.longitude;
-    });
+    //watch pra acompanhar a localização
+
+    navigator.geolocation.watchPosition(
+      (position) => {
+        lat.value = position.coords.latitude;
+        lng.value = position.coords.longitude;
+        //centraliza o mapa
+
+        if (map.value) {
+          // 1. Centraliza o mapa na nova posição
+          map.value.setView([lat.value, lng.value], 20);
+
+          // 2. Gerencia o marcador para não criar vários
+          if (marker.value) {
+            marker.value.setLatLng([lat.value, lng.value]);
+          } else {
+            marker.value = L.marker([lat.value, lng.value], { draggable: true })
+              .addTo(map.value)
+              .on('dragend', (event) => {
+                // Atualiza as refs se o usuário arrastar o ponto manualmente
+                const newPos = event.target.getLatLng();
+                lat.value = newPos.lat;
+                lng.value = newPos.lng;
+              });
+          }
+        }
+      },
+      (error) => {
+        console.error('Erro de geolocalização:', error);
+      },
+      {
+        enableHighAccuracy: true, // Melhora a precisão do ponto central
+      },
+    );
   }
 }
 </script>
--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -3,25 +3,24 @@
   <q-layout view="lHh Lpr lFf">
     <!--q page é usado p nao criar barra de rolagem e alinhar c os headers e footers do quasar-->
     <q-page-container>
-      <q-page class="flex q-pa-md q-pt-lg">
-        <div class="full-width full-height flex flex-col items-center">
-          <div class="text-center">
-            <span class="my-font tracking-normal">Olá, Luiza</span>
-            <p class="text-grey-7">Hoje 07 Jan</p>
-          </div>
+      <q-page class="column no-wrap overflow-hidden q-pa-md">
+        <div class="text-center q-pt-lg">
+          <span class="my-font tracking-normal">Olá, Luiza</span>
+          <p class="text-grey-7">Hoje 07 Jan</p>
+        </div>
 
-          <q-separator color="black" inset class="full-width" />
+        <q-separator color="black" inset class="full-width" />
 
-          <GoogleMaps />
-          <div class="full-width px-6 py-5">
-            <q-btn
-              :label="$t('index.btnStart')"
-              rounded
-              padding="16px 0"
-              class="full-width font-bold orange text-grey-8 my-font3"
-              to="/"
-            />
-          </div>
+        <GeoLocation />
+
+        <div class="full-width px-6 py-5">
+          <q-btn
+            :label="$t('index.btnStart')"
+            rounded
+            padding="16px 0"
+            class="full-width font-bold orange text-grey-8 my-font3"
+            to="/"
+          />
         </div>
         <!--ordem de hierarquia das page-->
       </q-page>
@@ -30,5 +29,5 @@
 </template>
 
 <script setup lang="ts">
-import GoogleMaps from 'src/components/GeoLocation.vue';
+import GeoLocation from 'src/components/GeoLocation.vue';
 </script>
