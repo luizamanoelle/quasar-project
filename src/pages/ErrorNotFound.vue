8de54d43d49bc4ac0de64d73a1143d8c372058ed
Luiza Manoelle <luizamanoelle@hotmail.com>
2026-01-20 12:55:12 -0300

    add page 404

--- src/pages/ErrorNotFound.vue
+++ src/pages/ErrorNotFound.vue
@@ -1,9 +1,12 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+  <div class="fullscreen bg-primary text-white text-center q-pa-md flex flex-center">
     <div>
-      <div style="font-size: 30vh">404</div>
+      <div style="font-size: 10vh">404</div>
 
-      <div class="text-h2" style="opacity: 0.4">Oops. Nothing here...</div>
+      <div class="w-full max-w-sm md:max-w-lg">
+        <q-img :src="noresults" class="md:scale-130" fit="contain" />
+      </div>
+      <div class="text-h3" style="opacity: 0.4">Oops. Nothing here...</div>
 
       <q-btn
         class="q-mt-xl"
@@ -19,5 +22,6 @@
 </template>
 
 <script setup lang="ts">
+import noresults from 'assets/noresults.svg';
 //
 </script>
